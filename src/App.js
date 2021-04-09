e0b012499ca0733adf715c262349d63d66209af1
Gustavo Fernando Balbino <gustavo.balbino@pca.com.br>
2021-04-09 17:48:15 -0300

    Adicionado página Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Home from './page-components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+
+		<Router>
+			<div>
+
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/users">Users</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/about">
+
+					</Route>
+					<Route path="/users">
+
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+
+	);
 }
 
 export default App;
